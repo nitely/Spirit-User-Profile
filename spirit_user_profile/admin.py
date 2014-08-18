9d2fece1411df794be508bcf84801cf2f3b5ff14
nitely <ochdownloader@gmail.com>
2014-08-18 17:01:22 -0300

    admin

--- spirit_user_profile/admin.py
+++ spirit_user_profile/admin.py
@@ -6,7 +6,8 @@ from .models import User
 
 
 class CustomUserAdmin(UserAdmin):
-
+    # You can use UserAdmin directly, this subclass adds administrator and moderator fields.
+    # If you are a superuser, just save your user in the admin and it'll automatically become an administrator.
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
